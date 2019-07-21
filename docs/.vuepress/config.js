e7cb74d7566eabc17db2786a0fd8751820e1cb1e
primavera <primaveraiiiiing618@gmail.com>
2019-07-21 17:35:35 +0800

    分离阿灯的blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: `pr1mavera`,
-    description: `pr1mavera's blog`,
+    description: `搞搞前端`,
     // host: '192.168.8.104',
     host: 'localhost',
     port: '8080',
@@ -22,64 +22,11 @@ module.exports = {
         sidebarDepth: 3,
         per_page: 6,
         date_format: 'yyyy-MM-dd HH:mm:ss',
-        // sidebar: [
-        //     '/blog/urlToPage',
-        //     '/blog/ha-js-sdk',
-        //     '/blog/RxJS'
-        // ],
         sidebar: {
             '/whatever/urlToPage': ['urlToPage'],
             '/whatever/ha-js-sdk': ['ha-js-sdk'],
             '/whatever/RxJS': ['RxJS'],
-            '/1.yd-pre/1.you-dont-know-html': ['1.you-dont-know-html'],
-            '/1.yd-pre/2.high-css': ['2.high-css'],
-            '/1.yd-pre/3.ES6-base': ['3.ES6-base'],
-            '/1.yd-pre/4.jQuery-core': ['4.jQuery-core'],
-            '/1.yd-pre/5.back-end': ['5.back-end']
-        },
-        nav: [
-            { text: '随便写写', link: '/tag/?tag=随便写写', icon: 'reco-blog' },
-            { text: '阿灯', link: '/tag/?tag=阿灯', icon: 'reco-edu' }
-        ]
-        // nav: [
-        //     { text: 'Tags', link: '/tags/', icon: 'reco-tag' },
-        //     { text: 'Blog', link: '/blog/', icon: 'reco-blog' }
-        //     // {
-        //     //     text: 'Blog',
-        //     //     items: [
-        //     //         { text: 'Tags', link: '/tags/', icon: 'reco-tag' },
-        //     //         { text: 'Blog', link: '/blog/', icon: 'reco-blog' }
-        //     //         // { text: 'Android', link: '/android/' },
-        //     //         // { text: 'ios', link: '/ios/' },
-        //     //         // { text: 'Web', link: '/web/' }
-        //     //     ]
-        //     // },
-        // 	// {
-        //     //     text: 'About',
-        //     //     link: '/about/'
-        //     // },
-        // ],
-        // nav: [
-        //     {
-        //         text: 'categories',
-        //         icon: 'reco-blog',
-        //         items: [
-        //             { text: 'frontEnd', link: '/categories/frontEnd' },
-        //             { text: 'backEnd', link: '/categories/backEnd' }
-        //         ]
-        //     },
-        //     { text: 'TimeLine', link: '/timeLine/', icon: 'reco-date' }
-        // ],
-        // 博客设置
-        // blogConfig: {
-        //     category: {
-        //         location: 2,     // 在导航栏菜单中所占的位置，默认2
-        //         text: 'Category' // 默认文案 “分类”
-        //     },
-        //     // tag: {
-        //     //     location: 3,     // 在导航栏菜单中所占的位置，默认3
-        //     //     text: 'Tag'      // 默认文案 “标签”
-        //     // }
-        // }
+            '/whatever/video-server': ['video-server']
+        }
     }
 }
