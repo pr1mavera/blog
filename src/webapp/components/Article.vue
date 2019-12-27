3161ced12a5b9a1ffd65c7af9b64e7edbb64f4df
primavera <pr1mavera.w4ng@gmail.com>
2019-12-27 23:25:48 +0800

    update

--- src/webapp/components/Article.vue
+++ src/webapp/components/Article.vue
@@ -5,7 +5,7 @@
     </aside>
     <!-- <vue-markdown :source="articleContent"></vue-markdown> -->
     <div class="main-wrapper">
-      <div class="container" v-html="content"></div>
+      <div class="container" v-html="this.articleContent[1]"></div>
       <!-- <div class="container">{{content}}</div> -->
     </div>
     <!-- <vue-markdown source="# 整体概念\n\n#### 对比工厂和工人\n+ 类似于一个工厂，有自己独立的资源（系统为进程分配的独立内存）\n+ 工厂之间相互独立（进程之间相互独立）\n+ 工厂中有一个或多个工人（进程管理一个或多个线程程）\n+ 多个工人协作完成任务（进程中的线程互相协作）\n+ 工人之间共享工厂资源（同一进程下的各个线程之间共享程序的内存空间：包括代码段、数据集、堆等）\n\n\n"></vue-markdown> -->
